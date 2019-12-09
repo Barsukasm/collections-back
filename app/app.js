7d00a56d8e54db9497798851f4b6a440175078bc
Kursant <Kursant@BOOK508wm8.win.ftc.ru>
2019-12-09 21:34:00 +0700

    added CORS

--- app/app.js
+++ app/app.js
@@ -8,6 +8,16 @@ const collectionRoutes = require('./routes/collections');
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(morgan('combined'));
 
 app.use('/collections', collectionRoutes);
